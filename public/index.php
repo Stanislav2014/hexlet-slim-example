4a0f114bf711f275748a142d10986b5b1f551004
stanislav <s_admin@Mail.ru>
2018-11-19 14:21:57 +0300

    add files

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$configuration = [
+    'settings' => [
+        'displayErrorDetails' => true,
+    ],
+];
+
+$app = new \Slim\App($configuration);
+$container = $app->getContainer();
+$container['renderer'] = new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+
+$app->get('/', function ($request, $response) {
+    return $response->write('Welcome to Slim!');
+});
+$app->get('/users/{id}', function ($request, $response, $args) {
+    $params = ['id' => $args['id']];
+    return $this->renderer->render($response, 'users/show.phtml', $params);
+});
+$app->run();
